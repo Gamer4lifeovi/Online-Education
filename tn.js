4150bf3b250e8f78eff52b8a489cf1136197b033
Gamer4lifeovi <109817878+Gamer4lifeovi@users.noreply.github.com>
2023-11-14 20:00:52 +0100

    Update tn.js

--- tn.js
+++ tn.js
@@ -2,9 +2,8 @@ let menuBtn = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .flex .navbar');
 
 menuBtn.onclick = () => {
-menuBtn.classList.toggle('fa-times');
-navbar.classList.toggle('active');
-
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 }
 
 
@@ -13,13 +12,13 @@ var swiper = new Swiper(".course-slider", {
     grabCursor:true,
     loop:true,
     pagination:{
-el: ".swiper-pagination",
-clickable:true,
+        el: ".swiper-pagination",
+        clickable:true,
     },
 
     breakpoints:{
         540:{
-            slidesPerView:1
+            slidesPerView:1,
         },
         768:{
             slidesPerView:2,
@@ -37,5 +36,39 @@ var swiper = new Swiper(".teachers-slider", {
     pagination:{
         el: ".swiper-pagination",
         clickable:true,
-    }
-})
+    },
+
+    breakpoints:{
+        540:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+    spaceBetween:20,
+    grabCursor:true,
+    loop:true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+
+    breakpoints:{
+        540:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    },
+});
